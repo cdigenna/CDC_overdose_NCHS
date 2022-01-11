0c615f157c6fb205b364fa305aff8f2fe0ae581e
CD239 <cdigennaro@mgh.harvard.edu>
2022-01-11 13:57:15 -0500

    updated analysis with 2020 data

--- NCHS_Code/VSYearMORT_Parser.py
+++ NCHS_Code/VSYearMORT_Parser.py
@@ -22,17 +22,18 @@
 #
 
 
-fileObj = open("VS19MORT.DUSMCPUB_r20210304",'r')
-fileOutObj = open("Data\VS19MORT.csv","a")
+fileObj = open("VS20MORT.DUSMCPUB_r20220105",'r')
+fileOutObj = open("Data\mort2020.csv","a")
 
-fileOutObj.write('Resident_Status, Education, Month_Of_Death, Sex, Age_Key, Age_Value, Age_Sub_Flag, Age_Recode_52, Age_Recode_27, ' + \
-                 'Age_Recode_12, Infant_Age_Recode_22, Place_Of_Death, Marital_Status, DOW_of_Death, Data_Year, Injured_At_Work, ' + \
-                 'Manner_Of_Death, Method_Of_Disposition, Autopsy, Activity_Code, Place_Of_Causal_Injury,  ICD10, Cause_Recode_358, ' + \
+fileOutObj.write('Resident_Status, Education, Month_Of_Death, sex, Age_Key, age, Age_Sub_Flag, ager52, Age_Recode_27, ' + \
+                 'Age_Recode_12, Infant_Age_Recode_22, placdth, Marital_Status, DOW_of_Death, year, Injured_At_Work, ' + \
+                 'Manner_Of_Death, Method_Of_Disposition, Autopsy, Activity_Code, Place_Of_Causal_Injury,  ucod, Cause_Recode_358, ' + \
                  'Cause_Recode_113, Infant_Cause_Recode_130, Cause_Recode_39, Entity_Axis_Conditions, EAC1, EAC2, EAC3, EAC4, EAC5, ' + \
                  'EAC6, EAC7, EAC8, EAC9, EAC10, EAC11, EAC12, EAC13, EAC14, EAC15, EAC16, EAC17, EAC18, EAC19, EAC20, ' + \
-                 'Record_Axis_Conditions, RA1, RA2, RA3, RA4, RA5, RA6, RA7, RA8, RA9, RA10, RA11, RA12, RA13, RA14, ' + \
-                 'RA15, RA16, RA17, RA18, RA19, RA20, Race, Race_Bridged, Race_Imputation, Race_Recode_3, Race_Recode_5, ' + \
-                 'Hispanic_Origin, Hispanic_Origin_Recode\n')
+                 'Record_Axis_Conditions, record_1, record_2, record_3, record_4, record_5, record_6, record_7, record_8, record_9, ' + \
+				 'record_10, record_11, record_12, record_13, record_14, record_15, record_16, record_17, record_18, record_19, record_20, ' + \
+				 'race, Race_Bridged, Race_Imputation, Race_Recode_3, Race_Recode_5, ' + \
+                 'hispanic, Hispanic_Origin_Recode\n')
 
 outStr = ""
 
@@ -41,25 +42,25 @@ for line in fileObj:
                Resident_Status = line[19].strip()
                Education = line[60:62].strip()
                Month_Of_Death = line[63:67].strip()
-               Sex = line[68].strip()
+               sex = line[68].strip()
                Age_Key = line[69].strip()
-               Age_Value = line[70:73].strip()
+               age = line[70:73].strip()
                Age_Sub_Flag = line[73].strip()
-               Age_Recode_52 = line[74:76].strip()
+               ager52 = line[74:76].strip()
                Age_Recode_27 = line[76:78].strip()
                Age_Recode_12 = line[78:80].strip()
                Infant_Age_Recode_22 = line[80:82].strip()
-               Place_Of_Death = line[82].strip()
+               placdth = line[82].strip()
                Marital_Status = line[83].strip()
                DOW_of_Death = line[84].strip()
-               Data_Year = line[101:105].strip()
+               year = line[101:105].strip()
                Injured_At_Work = line[105].strip()
                Manner_Of_Death = line[106].strip()
                Method_Of_Disposition = line[107].strip()
                Autopsy = line[108].strip()
                Activity_Code = line[143].strip()
                Place_Of_Causal_Injury = line[144].strip()
-               ICD10 = line[145:149].strip()
+               ucod = line[145:149].strip()
                Cause_Recode_358 = line[149:152].strip()
                Cause_Recode_113 = line[153:156].strip()
                Infant_Cause_Recode_130 = line[156:159].strip()
@@ -86,57 +87,57 @@ for line in fileObj:
                EAC19 = line[290:297].strip()
                EAC20 = line[297:304].strip()
                Record_Axis_Conditions = line[340:342]
-               RA1 = line[343:348].strip()
-               RA2 = line[348:353].strip()
-               RA3 = line[353:358].strip()
-               RA4 = line[358:363].strip()
-               RA5 = line[363:368].strip()
-               RA6 = line[368:373].strip()
-               RA7 = line[373:378].strip()
-               RA8 = line[378:383].strip()
-               RA9 = line[383:388].strip()
-               RA10 = line[388:393].strip()
-               RA11 = line[393:398].strip()
-               RA12 = line[398:403].strip()
-               RA13 = line[403:408].strip()
-               RA14 = line[408:413].strip()
-               RA15 = line[413:418].strip()
-               RA16 = line[418:423].strip()
-               RA17 = line[423:428].strip()
-               RA18 = line[428:433].strip()
-               RA19 = line[433:438].strip()
-               RA20 = line[438:443].strip()
-               Race = line[444:446].strip()
+               record_1 = line[343:348].strip()
+               record_2 = line[348:353].strip()
+               record_3 = line[353:358].strip()
+               record_4 = line[358:363].strip()
+               record_5 = line[363:368].strip()
+               record_6 = line[368:373].strip()
+               record_7 = line[373:378].strip()
+               record_8 = line[378:383].strip()
+               record_9 = line[383:388].strip()
+               record_10 = line[388:393].strip()
+               record_11 = line[393:398].strip()
+               record_12 = line[398:403].strip()
+               record_13 = line[403:408].strip()
+               record_14 = line[408:413].strip()
+               record_15 = line[413:418].strip()
+               record_16 = line[418:423].strip()
+               record_17 = line[423:428].strip()
+               record_18 = line[428:433].strip()
+               record_19 = line[433:438].strip()
+               record_20 = line[438:443].strip()
+               race = line[444:446].strip()
                Race_Bridged = line[446].strip()
                Race_Imputation = line[447].strip()
                Race_Recode_3 = line[448].strip()
                Race_Recode_5 = line[449].strip()
-               Hispanic_Origin = line[483:486].strip()
+               hispanic = line[483:486].strip()
                Hispanic_Origin_Recode = line[487].strip()
                
                
                
                
                
-               outStr = (Resident_Status +              ', ' + Education +              ', ' + Month_Of_Death +         ', ' + Sex + \
-                         ', ' + Age_Key +               ', ' + Age_Value +              ', ' + Age_Sub_Flag +           ', ' + Age_Recode_52 + \
-                         ', ' + Age_Recode_27 +         ', ' + Age_Recode_12 +          ', ' + Infant_Age_Recode_22 +   ', ' + Place_Of_Death + \
-                         ', ' + Marital_Status +        ', ' + DOW_of_Death +           ', ' + Data_Year +              ', ' + Injured_At_Work +  \
+               outStr = (Resident_Status +              ', ' + Education +              ', ' + Month_Of_Death +         ', ' + sex + \
+                         ', ' + Age_Key +               ', ' + age +              		', ' + Age_Sub_Flag +           ', ' + ager52 + \
+                         ', ' + Age_Recode_27 +         ', ' + Age_Recode_12 +          ', ' + Infant_Age_Recode_22 +   ', ' + placdth + \
+                         ', ' + Marital_Status +        ', ' + DOW_of_Death +           ', ' + year +              		', ' + Injured_At_Work +  \
                          ', ' + Manner_Of_Death +       ', ' + Method_Of_Disposition +  ', ' + Autopsy +                ', ' + Activity_Code + \
-                         ', ' + Place_Of_Causal_Injury +', ' + ICD10 +                  ', ' + Cause_Recode_358 +       ', ' + Cause_Recode_113 +  \
+                         ', ' + Place_Of_Causal_Injury +', ' + ucod +                  	', ' + Cause_Recode_358 +       ', ' + Cause_Recode_113 +  \
                          ', ' + Infant_Cause_Recode_130 + ', ' + Cause_Recode_39 +      ', ' + Entity_Axis_Conditions + ', ' + EAC1 + \
                          ', ' + EAC2 +                  ', ' + EAC3 +                   ', ' + EAC4 +                   ', ' + EAC5 + \
                          ', ' + EAC6 +                  ', ' + EAC7 +                   ', ' + EAC8 +                   ', ' + EAC9 + \
                          ', ' + EAC10 +                 ', ' + EAC11 +                  ', ' + EAC12 +                  ', ' + EAC13 + \
                          ', ' + EAC14 +                 ', ' + EAC15 +                  ', ' + EAC16 +                  ', ' + EAC17 + \
                          ', ' + EAC18 +                 ', ' + EAC19 +                  ', ' + EAC20 +                  ', ' + Record_Axis_Conditions + \
-                         ', ' + RA1 +                   ', ' + RA2 +                    ', ' + RA3 +                    ', ' + RA4 + \
-                         ', ' + RA5 +                   ', ' + RA6 +                    ', ' + RA7 +                    ', ' + RA8 + \
-                         ', ' + RA9 +                   ', ' + RA10 +                   ', ' + RA11 +                   ', ' + RA12 + \
-                         ', ' + RA13 +                  ', ' + RA14 +                   ', ' + RA15 +                   ', ' + RA16 + \
-                         ', ' + RA17 +                  ', ' + RA18 +                   ', ' + RA19 +                   ', ' + RA20 + \
-                         ', ' + Race +                  ', ' + Race_Bridged +           ', ' + Race_Imputation +        ', ' + Race_Recode_3 + \
-                         ', ' + Race_Recode_5 +         ', ' + Hispanic_Origin +        ', ' + Hispanic_Origin_Recode + '\n')
+                         ', ' + record_1 +              ', ' + record_2 +               ', ' + record_3 +               ', ' + record_4 + \
+                         ', ' + record_5 +              ', ' + record_6 +               ', ' + record_7 +               ', ' + record_8 + \
+                         ', ' + record_9 +              ', ' + record_10 +         		', ' + record_11 +              ', ' + record_12 + \
+                         ', ' + record_13 +             ', ' + record_14 +          	', ' + record_15 +              ', ' + record_16 + \
+                         ', ' + record_17 +             ', ' + record_18 +              ', ' + record_19 +             	', ' + record_20 + \
+                         ', ' + race +                  ', ' + Race_Bridged +           ', ' + Race_Imputation +        ', ' + Race_Recode_3 + \
+                         ', ' + Race_Recode_5 +         ', ' + hispanic +        		', ' + Hispanic_Origin_Recode + '\n')
 
 
                
